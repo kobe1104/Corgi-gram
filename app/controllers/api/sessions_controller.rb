2444ab65d1bd6a78fbb2b5e322f8abeb4fd937b3
Henry Hsu <henryhsu@Henrys-MacBook-Air.local>
2016-11-01 15:31:37 -0700

    session user controller done added api/users/show

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,29 @@
 class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(user_params[:username], user_params[:password])
+    if @user
+      log_in(@user)
+      render 'api/users/show'
+    else
+      render json: ["invalid credentials"], status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      log_out
+      render 'api/users/show'
+    else
+      render json: ["Current User does not exist"], status: 404
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
