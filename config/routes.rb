e5556c33402773007c3259cdd3e961ce2de4c9fe
Henry Hsu <henryhsu@Henrys-MacBook-Air.local>
2016-11-09 22:12:18 -0800

    test

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
 
   namespace :api, defaults: {format: :jason} do
-    resources :users, only: [:create, :index, :show]
+    resource :user, only: [:create, :index, :show]
     resource :session, only: [:create, :destroy, :show]
     resources :photos
     resources :comments
